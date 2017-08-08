da45b92f846707ee81e6cfbb45037a225e8b1fa6
HarishBal <harishbalakrishna@gmail.com>
2017-08-08 23:48:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -60,7 +60,9 @@ request.onreadystatechange=function()
             {
             // capture a list of names and render it as a list
             
-            var names=['name1', 'name2', 'name3', 'name4'];
+            var names=request.responsetext;
+            
+            names = JSON.parse();
             
             var list = '';
             for(var i=0; i < names.length; i++)
