4407e0125c06cff8d86a7da62d40f72e6bed4c8c
HarishBal <harishbalakrishna@gmail.com>
2017-08-08 16:27:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,9 +11,19 @@ element.innerHTML="New Value";
 
 var img = document.getElementById('madi');
 
+var margineLeft = 0;
+
+function moveRight()
+{
+    margineLeft = margineLeft + 10;
+    
+    img.style.marginLeft = margineLeft + 'px';
+}
+
 img.onclick = function(){
     console.log('Clicked!');
-    img.style.marginleft = '100px';
+    var interval = setInterval(moveRightt, 100);
+    //img.style.marginleft = '100px';
     
 };
 
