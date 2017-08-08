9b63b20d7dc8b2946b6b19ee00d585d1b943a224
HarishBal <harishbalakrishna@gmail.com>
2017-08-08 23:45:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,18 +46,40 @@ var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
     // Make a request to the server and send the name
-    // capture a list of names and render it as a list
-    var names=['name1', 'name2', 'name3', 'name4'];
-    
-    var list = '';
-    for(var i=0; i < names.length; i++)
-        {
-        list+= '<li>' + names[i] + '</li>';
-        }
+var request = new XMLHttpRequest();
 
-    var ul = document.getElementById('namelist');
+// Capture the response and store it in a variable
+
+request.onreadystatechange=function()
+    {
+    if  (request.readyState ===XMLHttpRequest.DONE)
+        {
+            //take some action
+            
+        if  (request.status ===200)
+            {
+            // capture a list of names and render it as a list
+            
+            var names=['name1', 'name2', 'name3', 'name4'];
+            
+            var list = '';
+            for(var i=0; i < names.length; i++)
+                {
+                list+= '<li>' + names[i] + '</li>';
+                }
+        
+            var ul = document.getElementById('namelist');
+            
+            ul.innerHTML = list;
+            }
+        }
+        // Not done yet
+    } ;   
     
-    ul.innerHTML = list;
+ // Make the request
+request.open('GET', 'http://harishbalakrishna.imad.hasura-app.io/submit-list', true);
+
+request.send(null);
     
     
 };
