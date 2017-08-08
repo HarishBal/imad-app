d1c011a84a1abc9f41d9a3562bf9e7c47af0d869
HarishBal <harishbalakrishna@gmail.com>
2017-08-08 23:05:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,7 +49,7 @@ submit.onclick=function()
 {
     // Make a request to the server and send the name
     // capture a list of names and render it as a list
-    var names=['name1', 'name2', 'name3'];
+    var names=['name1', 'name2', 'name3', 'name4'];
     
     var list = '';
     for(var i=0; i < name.length; i++)
@@ -58,6 +58,8 @@ submit.onclick=function()
         }
 
     var ul = document.getElementById('namelist');
+    
+    ul.innerHTML = list;
     
     
 };
