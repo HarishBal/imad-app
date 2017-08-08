825a0648a0d057890bdb2e672ebdb7ceba937147
HarishBal <harishbalakrishna@gmail.com>
2017-08-08 12:54:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,46 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
-var articleone={
-	title:"Article One", 
-	heading:"Article One",
-	date:"8 Aug 2017", 
+var articles={
+    articleone:{
+    	title:"Article One", 
+    	heading:"Article One",
+    	date:"8 Aug 2017", 
+    	content:`
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id nibh a mauris hendrerit tristique id quis libero. Vestibulum nec dolor sed arcu pretium molestie vel et tellus. Donec efficitur lorem quis tempus fringilla. Cras volutpat, tellus id malesuada ultrices, nisl neque feugiat urna, at accumsan dui eros id nisl. Nulla eleifend tortor in dolor lacinia, id aliquet odio eleifend. Sed aliquet feugiat dolor ut bibendum. Mauris sed nisl eu eros tristique tempor. Vivamus lacinia mi nunc, nec vulputate turpis maximus eu. Curabitur viverra lacus nec pellentesque tempor. Maecenas sed metus eget turpis facilisis congue a vitae justo. Integer vehicula suscipit convallis. Quisque fringilla aliquet velit. Nam bibendum vitae orci at elementum.
+                </p>
+                
+                <p>
+                    Proin vel dictum leo. Donec maximus lacus lectus, ut malesuada lorem maximus quis. Vestibulum in elementum tellus. Morbi interdum venenatis convallis. Mauris a ipsum elit. Integer rhoncus cursus ligula, id egestas augue sollicitudin vitae. Proin dui lectus, aliquam nec tortor eu, fringilla tempus lacus. Sed vitae mauris maximus, dignissim metus a, bibendum erat. Donec dictum nisi non hendrerit ullamcorper. Vestibulum facilisis nulla id lacus laoreet fermentum. Fusce maximus diam et mauris euismod molestie. Quisque vulputate nunc in tincidunt iaculis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur maximus orci mi, et auctor dui varius nec.
+                </p>
+                <p>
+                    Duis consectetur ligula velit, id rhoncus lacus convallis ut. Vivamus sed molestie nunc. Sed ut neque sollicitudin, ullamcorper magna eu, sollicitudin nunc. Donec non cursus urna, et varius nunc. Maecenas mi nunc, faucibus quis nisl non, hendrerit ornare lacus. Duis sit amet fringilla est. Nam nisl augue, interdum eget egestas at, vulputate eu leo. Aenean scelerisque tincidunt rutrum. Vivamus vitae nisl non nunc laoreet molestie vel vitae neque. Fusce vel ipsum ligula.
+                </p>
+                <p>
+                    Sed dapibus egestas libero, blandit convallis purus vulputate at. Duis consectetur ante eu purus vulputate sollicitudin. Donec fringilla orci purus. Aliquam egestas mi nec purus facilisis, dictum ultricies orci pellentesque. Fusce mi eros, mattis quis rhoncus vel, facilisis vitae diam. Etiam sed lectus a odio lobortis interdum. Vivamus magna justo, facilisis id tellus at, fermentum vestibulum quam. Nunc viverra, libero eu convallis hendrerit, velit lorem porta sapien, ut eleifend quam arcu non turpis. Fusce elementum porta diam, at fringilla quam pretium viverra. Donec feugiat aliquet fringilla. Fusce fringilla tempor tincidunt.
+                </p>
+                <p>
+                    Maecenas sit amet ligula porttitor, vehicula justo sed, interdum tellus. Curabitur vel est elit. Suspendisse ullamcorper, velit at fermentum euismod, sapien mi interdum nunc, sit amet vulputate elit mi sed massa. Donec tincidunt lectus non erat vehicula, eu venenatis purus accumsan. Curabitur aliquet commodo velit sed ultricies. Etiam gravida suscipit nunc sit amet faucibus. Ut finibus massa eget orci ultrices, ac convallis elit facilisis. Nam tincidunt ipsum et egestas venenatis. Proin enim nibh, ultricies nec porta gravida, egestas in mi. Aenean et felis massa.
+                </p>`
+        },
+    articletwo:{
+	title:"Article Two", 
+	heading:"Article Two",
+	date:"10 Aug 2017", 
+	content:`
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id nibh a mauris hendrerit tristique id quis libero. Vestibulum nec dolor sed arcu pretium molestie vel et tellus. Donec efficitur lorem quis tempus fringilla. Cras volutpat, tellus id malesuada ultrices, nisl neque feugiat urna, at accumsan dui eros id nisl. Nulla eleifend tortor in dolor lacinia, id aliquet odio eleifend. Sed aliquet feugiat dolor ut bibendum. Mauris sed nisl eu eros tristique tempor. Vivamus lacinia mi nunc, nec vulputate turpis maximus eu. Curabitur viverra lacus nec pellentesque tempor. Maecenas sed metus eget turpis facilisis congue a vitae justo. Integer vehicula suscipit convallis. Quisque fringilla aliquet velit. Nam bibendum vitae orci at elementum.
+            </p>
+            
+              <p>
+                Maecenas sit amet ligula porttitor, vehicula justo sed, interdum tellus. Curabitur vel est elit. Suspendisse ullamcorper, velit at fermentum euismod, sapien mi interdum nunc, sit amet vulputate elit mi sed massa. Donec tincidunt lectus non erat vehicula, eu venenatis purus accumsan. Curabitur aliquet commodo velit sed ultricies. Etiam gravida suscipit nunc sit amet faucibus. Ut finibus massa eget orci ultrices, ac convallis elit facilisis. Nam tincidunt ipsum et egestas venenatis. Proin enim nibh, ultricies nec porta gravida, egestas in mi. Aenean et felis massa.
+            </p>`
+},
+    articlethree:{
+	title:"Article Three", 
+	heading:"Article Three",
+	date:"10 Aug 2017", 
 	content:`
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id nibh a mauris hendrerit tristique id quis libero. Vestibulum nec dolor sed arcu pretium molestie vel et tellus. Donec efficitur lorem quis tempus fringilla. Cras volutpat, tellus id malesuada ultrices, nisl neque feugiat urna, at accumsan dui eros id nisl. Nulla eleifend tortor in dolor lacinia, id aliquet odio eleifend. Sed aliquet feugiat dolor ut bibendum. Mauris sed nisl eu eros tristique tempor. Vivamus lacinia mi nunc, nec vulputate turpis maximus eu. Curabitur viverra lacus nec pellentesque tempor. Maecenas sed metus eget turpis facilisis congue a vitae justo. Integer vehicula suscipit convallis. Quisque fringilla aliquet velit. Nam bibendum vitae orci at elementum.
@@ -20,15 +55,9 @@ var articleone={
             </p>
             <p>
                 Duis consectetur ligula velit, id rhoncus lacus convallis ut. Vivamus sed molestie nunc. Sed ut neque sollicitudin, ullamcorper magna eu, sollicitudin nunc. Donec non cursus urna, et varius nunc. Maecenas mi nunc, faucibus quis nisl non, hendrerit ornare lacus. Duis sit amet fringilla est. Nam nisl augue, interdum eget egestas at, vulputate eu leo. Aenean scelerisque tincidunt rutrum. Vivamus vitae nisl non nunc laoreet molestie vel vitae neque. Fusce vel ipsum ligula.
-            </p>
-            <p>
-                Sed dapibus egestas libero, blandit convallis purus vulputate at. Duis consectetur ante eu purus vulputate sollicitudin. Donec fringilla orci purus. Aliquam egestas mi nec purus facilisis, dictum ultricies orci pellentesque. Fusce mi eros, mattis quis rhoncus vel, facilisis vitae diam. Etiam sed lectus a odio lobortis interdum. Vivamus magna justo, facilisis id tellus at, fermentum vestibulum quam. Nunc viverra, libero eu convallis hendrerit, velit lorem porta sapien, ut eleifend quam arcu non turpis. Fusce elementum porta diam, at fringilla quam pretium viverra. Donec feugiat aliquet fringilla. Fusce fringilla tempor tincidunt.
-            </p>
-            <p>
-                Maecenas sit amet ligula porttitor, vehicula justo sed, interdum tellus. Curabitur vel est elit. Suspendisse ullamcorper, velit at fermentum euismod, sapien mi interdum nunc, sit amet vulputate elit mi sed massa. Donec tincidunt lectus non erat vehicula, eu venenatis purus accumsan. Curabitur aliquet commodo velit sed ultricies. Etiam gravida suscipit nunc sit amet faucibus. Ut finibus massa eget orci ultrices, ac convallis elit facilisis. Nam tincidunt ipsum et egestas venenatis. Proin enim nibh, ultricies nec porta gravida, egestas in mi. Aenean et felis massa.
             </p>`
+    }
 };
-
 function createTemplate(data)
     {
     var title=data.title;
@@ -75,6 +104,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/:articleName', function (req, res) {
+  //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
+});
+
+/*
+
 app.get('/ui/article-one', function (req, res) {
   //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
   
@@ -88,6 +125,7 @@ app.get('/ui/article-two', function (req, res) {
 app.get('/ui/article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
+*/
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
