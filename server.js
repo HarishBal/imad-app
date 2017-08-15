d6191ea22407c57defae1233efaaa0a722b1c5ef
HarishBal <harishbalakrishna@gmail.com>
2017-08-15 16:39:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -195,6 +195,28 @@ app.get('/articles/:articleName', function (req, res) {
   });
   
 });
+
+app.get('/articles', function (req, res) {
+
+  pool.query("SELECT * from articles", function(err, result){
+      if(err){
+        res.status(500).send(err.toString());
+      } else{
+            if(result.rows.length ===0){
+               res.status(404).send('Article not Found'); 
+            } else {
+                var artileData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+      } 
+  });
+  
+});
+
+
+
+
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
