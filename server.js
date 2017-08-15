566fe6dbbb925c5601322c2d49ea81466954a065
HarishBal <harishbalakrishna@gmail.com>
2017-08-15 16:44:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -188,7 +188,7 @@ app.get('/articles/:articleName', function (req, res) {
             if(result.rows.length ===0){
                res.status(404).send('Article not Found'); 
             } else {
-                var artileData = result.rows[0];
+                var articleData = result.rows[0];
                 res.send(createTemplate(articleData));
             }
       } 
